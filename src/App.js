c43e59c914ae7ddaa6abf38d232a2cffd5d6c5a7
Your Name <you@example.com>
2022-12-15 03:59:43 +0400

    H2 Typography Component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 //import Card from './Card.js';
 // import Button from './components/Button';
 import H1 from './components/H1.js'
+import H2 from './components/H2.js'
 
 function App() {
   return (
     <div className="App">
     <H1/>
+    <H2/>
     </div>
   );
 }
---
+++ src/components/H2.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./H2.module.scss";
+
+const H2 = () => {
+  return (
+    <p className={styles.text}>
+      Unlimited movies, TV shows, and more.
+    </p>
+  );
+};
+
+export default H2;
