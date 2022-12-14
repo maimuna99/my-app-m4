577eadb3d8449e42c9c91faa51d19a2ae2dd6a70
Your Name <you@example.com>
2022-12-15 03:48:22 +0400

    H1 Typography Component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 // import logo from './logo.svg';
 import './App.css';
-import Button from './Button';
 //import Card from './Card.js';
 // import Button from './components/Button';
-//import H1 from './components/H1.js'
+import H1 from './components/H1.js'
 
 function App() {
   return (
     <div className="App">
-    
-      <Button/>
+    <H1/>
     </div>
   );
 }
---
+++ src/components/H1.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./H1.module.scss";
+
+const H1 = () => {
+  return (
+    <p className={styles.text}>
+      Unlimited movies, TV shows, and more.
+    </p>
+  );
+};
+
+export default H1;
